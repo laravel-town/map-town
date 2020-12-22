235b2a7280c89cb60e198840b8ddf6ec469876d7
Nuno Maduro <enunomaduro@gmail.com>
2020-12-22 22:41:12 +0100

    wip

---
+++ nuno.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="nuno" tilewidth="32" tileheight="32" tilecount="4" columns="2">
+ <image source="../../../Desktop/nuno.jpg" width="64" height="64"/>
+</tileset>
---
+++ tilesets/Slates v.2 [32x32px orthogonal tileset by Ivan Voirol].tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Slates v.2 [32x32px orthogonal tileset by Ivan Voirol]" tilewidth="32" tileheight="32" tilecount="1288" columns="56">
+ <image source="../../../Downloads/Slates v.2 [32x32px orthogonal tileset by Ivan Voirol].png" width="1792" height="736"/>
+</tileset>
